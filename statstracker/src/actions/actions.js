8e93d4777b54af00f828e6be792a534c683bafb8
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-09 16:44:53 -0400

    Using Redux. Added redux and react-redux. Wrote reducers and action creators.

---
+++ statstracker/src/actions/actions.js
@@ -0,0 +1,9 @@
+export const STRIKE = 'STRIKE';
+export const BALL = 'BALL';
+export const FOUL = 'FOUL';
+export const HIT = 'HIT';
+
+export const strike = () => ({ type: STRIKE });
+export const ball = () => ({ type: BALL });
+export const foul = () => ({ type: FOUL });
+export const hit = () => ({ type: HIT });
