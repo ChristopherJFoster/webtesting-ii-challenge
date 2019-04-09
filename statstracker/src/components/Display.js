8d0495552ba794de49f484109a10ef4467ad5ec7
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-09 18:44:47 -0400

    Strike button test works now. Learned a bit more about regex.

--- statstracker/src/components/Display.js
+++ statstracker/src/components/Display.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 const Display = ({ balls, strikes }) => {
   return (
     <div className='display'>
-      <h2>Balls: {balls}</h2>
-      <h2>Strikes: {strikes}</h2>
+      <h2 id='display-balls'>Balls: {balls}</h2>
+      <h2 id='display-strikes'>Strikes: {strikes}</h2>
     </div>
   );
 };
--- statstracker/src/tests/App.test.js
+++ statstracker/src/tests/App.test.js
@@ -1,23 +1,37 @@
+// Required to get <App /> running:
 import React from 'react';
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import rootReducer from '../reducers/reducers';
-
 import App from '../components/App';
-
 const store = createStore(rootReducer);
 
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import 'jest-dom/extend-expect';
+
+const packagedApp = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 describe('<App />', () => {
   it('renders successfully', () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
+    render(packagedApp);
+  });
+
+  it('adds a strike 0 to 1', () => {
+    const { getByText } = render(packagedApp);
+    const button = getByText(/strike$/i);
+    fireEvent.click(button);
+    getByText(/strikes: 1/i);
+  });
+
+  it('resets count on 3rd strike', () => {
+    const { getByText } = render(packagedApp);
+    const button = getByText(/strike$/i);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    getByText(/strikes: 0/i);
   });
 });
